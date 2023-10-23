907ce2ad7e6645a6d6f5088bbaf63e07c3246722
Rahman <afsana.rahman@fiserv.com>
2023-10-23 10:24:46 -0400

    java-ii ex1 complete

--- src/main/java/com/xpanxion/assignments/Main.java
+++ src/main/java/com/xpanxion/assignments/Main.java
@@ -4,6 +4,7 @@ import com.xpanxion.assignments.shared.*;
 import com.xpanxion.assignments.student.JavaOne;
 
 import java.text.NumberFormat;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -20,6 +21,23 @@ public class Main {
 //        javaOne.ex9();
 //        javaOne.ex10();
 
+        Scanner console = new Scanner(System.in);
+        ArrayList<Person> personList = new ArrayList<Person>();
+        String input = "";
+        while(!input.equals("done")) {
+            System.out.print("Enter person: ");
+            input = console.nextLine();
+            if(!input.equals("done")) {
+                String[] tokens = input.split(" ");
+                int id = Integer.parseInt(tokens[0].substring(0, tokens[0].length()-1));
+                personList.add(new Person(id, tokens[1], tokens[2]));
+            }
+        }
+
+        for(Person person : personList) {
+            System.out.println(person.toString());
+        }
+
 //        Invoice invoice =  new Invoice(1);
 //        invoice.addProduct(new Product(111,"Mustard", 2.00));
 //        invoice.addProduct(new Product(222,"Ketchup", 3.00));
@@ -27,19 +45,18 @@ public class Main {
 //        NumberFormat formatter = NumberFormat.getCurrencyInstance();
 //        System.out.println("Total cost: " + formatter.format(invoice.getTotalCost()));
 
-//        Scanner console = new Scanner(System.in);
 //        Calculator calculator = new Calculator();
 //        calculator.calculate(console);
 
-        CatQueue q = new CatQueue();
-        q.enqueue(new Cat("Alice"));
-        q.enqueue(new Cat("Bob"));
-        q.enqueue(new Cat("Charlie"));
-        q.enqueue(new Cat("Dan"));
-        while(!q.isEmpty()) {
-            q.printQueue();
-            q.dequeue();
-            Thread.sleep(3000);
-        }
+//        CatQueue q = new CatQueue();
+//        q.enqueue(new Cat("Alice"));
+//        q.enqueue(new Cat("Bob"));
+//        q.enqueue(new Cat("Charlie"));
+//        q.enqueue(new Cat("Dan"));
+//        while(!q.isEmpty()) {
+//            q.printQueue();
+//            q.dequeue();
+//            Thread.sleep(3000);
+//        }
     }
 }
---
+++ src/main/java/com/xpanxion/assignments/shared/Person.java
@@ -0,0 +1,17 @@
+package com.xpanxion.assignments.shared;
+
+public class Person {
+    private int id;
+    private String firstName;
+    private String lastName;
+
+    public Person(int id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String toString() {
+        return "Person{id=" + id + ", firstName='" + firstName + "', lastName='" + lastName + "'}";
+    }
+}
